c92d72dc20238d6a5b8498e35b64f388037c4fe4
Caleb Flynn <calebflynn13@gmail.com>
2021-06-29 20:15:42 -0500

    one more test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        System.out.println("It's been a while since using IntelliJ.\nLet's make sure this works.");
     }
 }
