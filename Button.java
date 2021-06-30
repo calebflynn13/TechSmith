0c9f7ba5f4291d0281fbf4471a5e887b76818ec1
Caleb Flynn <calebflynn13@gmail.com>
2021-06-29 22:35:57 -0500

    add number buttons to screen

---
+++ Button.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+
+public class Button {
+    private int x, y, width, height;
+    private String value;
+
+    public Button(String strValue, int screenWidth, int screenHeight)
+    {
+        int value = Integer.parseInt(strValue);
+        if (value == 0)
+        {
+            this.x = ((screenWidth - 150) / 3) + 20;
+            this.y = 300 + 120 * 3;
+        }
+        else
+        {
+            this.x = ((value - 1) % 3) * ((screenWidth - 150) / 3) + 20;
+            this.y = 300 + 120 * ((value - 1) / 3);
+        }
+        this.width = 100;
+        this.height = 100;
+        this.value = strValue;
+    }
+
+    public void paint(Graphics g)
+    {
+        int fontSize = 50;
+        g.setColor(Color.white);
+        g.fillRect(x, y, width, height);
+        g.setColor(Color.black);
+        g.setFont(new Font("Arial", Font.PLAIN, fontSize));
+        g.drawString(value, x + width / 2 - fontSize / 4, y + height / 2 + fontSize / 4);
+    }
+
+}
--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 
 /**
@@ -9,19 +10,31 @@ public class Calculator extends Canvas{
     public static int WIDTH = 500;
     public static int HEIGHT = 800;
     public static int padding = 10;
+    public static Computer computer = new Computer();
+
+    public static void main(String[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        JFrame frame = new JFrame();
+        frame.add(calculator);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setVisible(true);
+    }
 
     public void paint(Graphics g) {
         setBackground(Color.WHITE);
         g.setColor(Color.gray);
         g.fillRoundRect(padding, padding, getWidth() - padding * 2, getHeight() - padding * 2, 10, 10);
-    }
 
-    public static void main(String[] args)
-    {
-        Calculator calculator = new Calculator();
-        JFrame frame = new JFrame();
-        frame.add(calculator);
-        frame.setSize(WIDTH, HEIGHT);
-        frame.setVisible(true);
+        // buttons
+        ArrayList<Button> buttons = new ArrayList<Button>();
+        for (int i = 0; i <= 9; i++)
+        {
+            buttons.add(new Button("" + i, getWidth(), getHeight()));
+        }
+        for (Button button : buttons) {
+            button.paint(g);
+        }
     }
 }
